205b88da9b40c8c68e27659d8a1fbc54100a4b6d
Nilan <nilan.marktanner@student.kit.edu>
2015-12-30 00:59:50 +0100

    Add Constants singleton

---
+++ app/src/main/java/co/optonaut/optonaut/util/Constants.java
@@ -0,0 +1,35 @@
+package co.optonaut.optonaut.util;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.DisplayMetrics;
+
+/**
+ * @author Nilan Marktanner
+ * @date 2015-12-30
+ */
+public class Constants {
+    private static Constants constants;
+
+    private DisplayMetrics displayMetrics;
+
+    private Constants(Activity activity) {
+        displayMetrics = new DisplayMetrics();
+        activity.getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+    }
+
+    public static void initializeConstants(Activity activity) {
+        constants = new Constants(activity);
+    }
+
+    public static Constants getInstance() {
+        if (constants == null) {
+            throw new RuntimeException();
+        }
+        return constants;
+    }
+
+    public DisplayMetrics getDisplayMetrics() {
+        return displayMetrics;
+    }
+}
