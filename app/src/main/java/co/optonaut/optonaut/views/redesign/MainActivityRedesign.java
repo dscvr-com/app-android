42bef77b08724cf90eef891dfd52afe3831873b9
Nilan <nilan.marktanner@student.kit.edu>
2016-01-06 20:31:13 +0100

    Playing around with Status bar and stable layout.

--- app/src/main/java/co/optonaut/optonaut/views/redesign/MainActivityRedesign.java
+++ app/src/main/java/co/optonaut/optonaut/views/redesign/MainActivityRedesign.java
@@ -2,6 +2,7 @@ package co.optonaut.optonaut.views.redesign;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import co.optonaut.optonaut.R;
 import co.optonaut.optonaut.util.Constants;
@@ -30,6 +31,32 @@ public class MainActivityRedesign extends AppCompatActivity {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.feed_placeholder, mainSnappyFeedFragment).commit();
         }
+
+        goFullscreen();
+    }
+
+    private void goFullscreen() {
+        // go full fullscreen mode
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        );
+
+        /*
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+        */
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        goFullscreen();
     }
 
     @Override
