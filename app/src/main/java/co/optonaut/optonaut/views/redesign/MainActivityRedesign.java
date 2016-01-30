5836090c16903cca7319a0cebe899dfd94c0b3fc
Nilan <nilan.marktanner@student.kit.edu>
2016-01-30 20:22:27 +0100

    Modified fullscreen mode flags.

--- app/src/main/java/co/optonaut/optonaut/views/redesign/MainActivityRedesign.java
+++ app/src/main/java/co/optonaut/optonaut/views/redesign/MainActivityRedesign.java
@@ -58,15 +58,7 @@ public class MainActivityRedesign extends AppCompatActivity {
         // go full fullscreen mode
         getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
         );
-
-        /*
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
-        */
     }
 
     private void hideStatusBar() {
