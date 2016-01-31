c6b1b3512c40ad4d11f6bda2d8386485527e3cfd
Nilan <nilan.marktanner@student.kit.edu>
2016-01-31 02:17:40 +0100

    Minor refactoring

--- app/src/main/java/co/optonaut/optonaut/util/ImageUrlBuilder.java
+++ app/src/main/java/co/optonaut/optonaut/util/ImageUrlBuilder.java
@@ -2,7 +2,6 @@ package co.optonaut.optonaut.util;
 
 import android.opengl.GLES20;
 import android.util.Base64;
-import android.util.Log;
 
 import java.io.UnsupportedEncodingException;
 import java.security.InvalidKeyException;
