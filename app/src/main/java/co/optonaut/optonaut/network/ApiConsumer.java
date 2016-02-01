edaecef6ed7172db49b79b85a2cab8b8dbf4700c
Nilan <nilan.marktanner@student.kit.edu>
2016-02-01 15:11:51 +0100

    Changing server back to staging.

--- app/src/main/java/co/optonaut/optonaut/network/ApiConsumer.java
+++ app/src/main/java/co/optonaut/optonaut/network/ApiConsumer.java
@@ -27,8 +27,7 @@ import timber.log.Timber;
  * @date 2015-11-13
  */
 public class ApiConsumer {
-    // private static final String BASE_URL = "https://api-staging.optonaut.co/";
-    private static final String BASE_URL = "http://optonaut.ngrok.io";
+    private static final String BASE_URL = "https://api-staging.optonaut.co/";
     private static final String TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImQyYmVhNmI3LWQxYzktNDEyMi04YTJmLTlkMDFmNTAzZjY2ZCJ9._sVJmnCvSyDeoxoSaD4EkEGisyblUvkb1PufUz__uOY";
 
     private static final int DEFAULT_LIMIT = 5;
