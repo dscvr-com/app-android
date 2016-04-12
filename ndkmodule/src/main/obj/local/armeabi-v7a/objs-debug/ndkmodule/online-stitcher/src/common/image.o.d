38d17de8f14fbfb601c1a11880b7404f23cfb4fb
Mariel <mtrillana@thousandminds.com>
2016-04-12 13:45:39 +0800

    Commit: delete libs,obj,online-stitcher

--- ndkmodule/src/main/obj/local/armeabi-v7a/objs-debug/ndkmodule/online-stitcher/src/common/image.o.d
+++
@@ -1,2 +0,0 @@
-C:/Users/James/AndroidStudioProjects/app-android/ndkmodule/src/main/obj/local/armeabi-v7a/objs-debug/ndkmodule/online-stitcher/src/common/image.o: \
- C:/Users/James/AndroidStudioProjects/app-android/ndkmodule/src/main/jni/online-stitcher/src/common/image.cpp
