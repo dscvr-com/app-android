28de572a035df31b564b65cc90130e0e03d93f7e
marckuz <maelbambo@gmail.com>
2016-09-07 17:22:13 +0800

    - node detector

--- app/src/main/java/com/iam360/iam360/opengl/Optograph2DCubeRenderer.java
+++ app/src/main/java/com/iam360/iam360/opengl/Optograph2DCubeRenderer.java
@@ -12,7 +12,6 @@ import com.iam360.iam360.storytelling.MarkerNode;
 import com.iam360.iam360.util.Constants;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import javax.microedition.khronos.egl.EGLConfig;
@@ -29,18 +28,20 @@ public class Optograph2DCubeRenderer implements GLSurfaceView.Renderer {
     private static final float FIELD_OF_VIEW_Y_ZOOM = 70.0f;
     private static final float Z_NEAR = 0.1f;
     private static final float Z_FAR = 120.0f;
+    private static final float V_DISTANCE = 20f;
     private float scaleFactor = 1.f;
     private float ratio = 1.f;
 
     private static final float DAMPING_FACTOR = 0.9f;
 
     private final float[] mvpMatrix = new float[16];
-    private final float[] mvpMatrix2 = new float[16];
 
     private final float[] projection = new float[16];
     private final float[] camera = new float[16];
     private float[] rotationMatrix = new float[16];
 
+    private float[] unInverseRotationMatrix = new float[16];
+    private float sphereRadius = 50f;
     private CombinedMotionManager combinedMotionManager;
 
     private Cube cube;
@@ -62,12 +63,12 @@ public class Optograph2DCubeRenderer implements GLSurfaceView.Renderer {
         Timber.v("onSurfaceCreated");
         this.cube.initialize();
 
-        sphere = new MarkerNode(5, 2);
+        sphere = new MarkerNode(5, sphereRadius);
         sphere.initializeProgram();
         setSpherePosition(2.0f, 1.0f, 2.0f);
 
         for(int a=0; a < 20; a++){
-            spheres.add(new MarkerNode(5,5));
+            spheres.add(new MarkerNode(5,sphereRadius));
             spheres.get(spheres.size() - 1).initializeProgram();
             spheres.get(spheres.size() - 1).setTransform(sphere.getTransform());
             spheres.get(spheres.size() - 1).setInitiliazed(false);
@@ -75,9 +76,9 @@ public class Optograph2DCubeRenderer implements GLSurfaceView.Renderer {
 
         // Set the camera position
         Matrix.setLookAtM(camera, 0,
-                0.0f, 0.0f, 0.0f, // eye
-                0.0f, 0.0f, 0.01f, // center
-                0.0f, 1.0f, 0.0f); // up
+                0.0f, 0.0f, 0.0f, // eye; location
+                0.0f, 0.0f, 0.01f, // center; looking toward center
+                0.0f, 1.0f, 0.0f); // up; pointing along Y-axis
 
         // Set the background frame color
         GLES20.glClearColor(0.0f, 1.0f, 0.0f, 1.0f);
@@ -94,7 +95,7 @@ public class Optograph2DCubeRenderer implements GLSurfaceView.Renderer {
                 0, 0, 0.01f, 0,
                 x, y, z, 1
         });
-        Log.d("MARK2","setSpherePosition x="+x+"  y="+y+"  z="+z);
+//        Log.d("MARK2","setSpherePosition x="+x+"  y="+y+"  z="+z);
     }
 
     @Override
@@ -109,34 +110,60 @@ public class Optograph2DCubeRenderer implements GLSurfaceView.Renderer {
     @Override
     public void onDrawFrame(GL10 gl) {
         TouchEventListener touchEventListener = combinedMotionManager.getTouchEventListener();
+        float currTheta = touchEventListener.getTheta();
+        float currPhi = touchEventListener.getPhi();
+//        double longRadians = Math.toRadians(currTheta);
+//        double latRadians = Math.toRadians(currPhi);
+
+        //x=rsinφcosθ; y=rsinφsinθ; z=rcosφ
+//        float x = (float) (1f * (Math.sin(currPhi) * Math.cos(currTheta)));
+//        float y = (float) (1f * (Math.sin(currPhi) * Math.sin(currTheta)));
+//        float z = (float) (1f * Math.cos(currPhi));
+
+
+        double xy_distance = V_DISTANCE * Math.cos(currPhi);
+        float x = (float) (xy_distance * Math.cos(currTheta));
+        float y = (float) (V_DISTANCE * Math.sin(currPhi));
+        float z = (float) (xy_distance * Math.sin(currTheta));
+
+
+        Log.d("currPhicurrTheta","x = "+x+"  y = "+y+"  z = "+z);
+
+        unInverseRotationMatrix = combinedMotionManager.getRotationMatrix();
+        float[] vector = {0, 0, V_DISTANCE, 0};
+        float[] newPosition = new float[4];
+        Matrix.multiplyMV(newPosition, 0, unInverseRotationMatrix, 0, vector, 0);
 
         // rotate viewMatrix to allow for user-interaction
         float[] view = new float[16];
         rotationMatrix = combinedMotionManager.getRotationMatrixInverse();
         Matrix.multiplyMM(view, 0, camera, 0, rotationMatrix, 0);
-        Log.d("MARK3","view = "+ Arrays.toString(view));
 
         if (optoType!=null && optoType.equals("optograph_1")) Matrix.perspectiveM(projection, 0, FIELD_OF_VIEW_Y_ZOOM / scaleFactor, ratio, Z_NEAR, Z_FAR);
         else Matrix.perspectiveM(projection, 0, FIELD_OF_VIEW_Y / scaleFactor, ratio, Z_NEAR, Z_FAR);
 
-
         // Calculate the projection and view transformation
         Matrix.multiplyMM(mvpMatrix, 0, projection, 0, view, 0);
 
-        float[] invertMvp = new float[16];
-        Matrix.invertM(invertMvp, 0, mvpMatrix, 0);
+        Log.d("onDrawFrame","newPosition[0] = "+ newPosition[0]+"  newPosition[1] = "+newPosition[1]+"  newPosition[2] = "+newPosition[2]);
 
-        Log.d("MARK3","invertMvp = "+ Arrays.toString(invertMvp));
-
-        setSpherePosition(touchEventListener.getPhi(),touchEventListener.getTheta(),invertMvp[14]);
+        setSpherePosition(newPosition[0], newPosition[1], newPosition[2]);
 
         // Draw shape
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
         cube.draw(mvpMatrix);
-        Log.d("MARK3","mvpMatrix = "+ Arrays.toString(mvpMatrix));
+
         sphere.draw(mvpMatrix);
+        Log.d("currPhicurrTheta","currPhi = "+currPhi+"  currTheta = "+currTheta);
+//        sphere.setCenter(currPhi, currTheta, 1f);
+
+
+        sphere.setCenter(x, y, z);
+//        sphere.setCenter(newPosition[0], newPosition[1], newPosition[2]);
+
         for(int a=0; a< spheres.size(); a++){
             if(spheres.get(a).isInitiliazed()){
+                Log.d("MARK2","overlapSpheres trufalse = "+overlapSpheres(sphere, spheres.get(a)));
                 spheres.get(a).draw(mvpMatrix);
             }
         }
@@ -201,6 +228,8 @@ public class Optograph2DCubeRenderer implements GLSurfaceView.Renderer {
                 spheres.get(a).setMarkerName("text-"+a);
 //                Log.d("MARK","addMarker = "+ Arrays.toString(sphere.getTransform()));
                 spheres.get(a).setTransform(sphere.getTransform());
+                Log.d("MARK2","overlapSpheres addMarker .x="+sphere.getCenter().x+"  .y="+sphere.getCenter().y+"  .z="+sphere.getCenter().z);
+                spheres.get(a).setCenter(sphere.getCenter().x, sphere.getCenter().y, sphere.getCenter().z);
                 spheres.get(a).setInitiliazed(true);
                 break;
             }
@@ -209,28 +238,21 @@ public class Optograph2DCubeRenderer implements GLSurfaceView.Renderer {
 
     public static boolean overlapSpheres(Sphere point, Sphere marker) {
         // we are using multiplications because it's faster than calling Math.pow
-//        double distance = Math.sqrt((point.center.x - marker.center.x) * (point.center.x - marker.center.x) +
-//                (point.center.y - marker.center.y) * (point.center.y - marker.center.y) +
-//                (point.center.z - marker.center.z) * (point.center.z - marker.center.z));
 
+        Log.d("MARK2","overlapSpheres point.center.x="+point.getCenter().x+"  point.center.y="+point.getCenter().y+"  point.center.z="+point.getCenter().z);
+        Log.d("MARK2","overlapSpheres marker.center.x="+marker.getCenter().x+"  marker.center.y="+marker.getCenter().y+"  marker.center.z="+marker.getCenter().z);
 
-        Log.d("MARK2","overlapSpheres point.center.x="+point.center.x+"  point.center.y="+point.center.y+"  point.center.z="+point.center.z);
-        Log.d("MARK2","overlapSpheres marker.center.x="+marker.center.x+"  marker.center.y="+marker.center.y+"  marker.center.z="+marker.center.z);
-        double distance = Math.sqrt((point.center.x - marker.center.x) * (point.center.x - marker.center.x) +
-                (point.center.y - marker.center.y) * (point.center.y - marker.center.y) +
-                (point.center.z - marker.center.z) * (point.center.z - marker.center.z));
+        float x, y, z;
+        x = point.getCenter().x - marker.getCenter().x;
+        y = point.getCenter().y - marker.getCenter().y;
+        z = point.getCenter().z - marker.getCenter().z;
 
-//        float x, y, z;
-//        x = point.center.x - marker.center.x;
-//        y = point.center.y - marker.center.y;
-//        z = point.center.z - marker.center.z;
-//
-//        double distance = Math.sqrt( x*x + y*y + z*z );
+        double distance = Math.sqrt( x*x + y*y + z*z );
 
         Log.d("MARK2","distance == "+distance);
-        Log.d("MARK2","Math.sqrt(marker.radius) == "+marker.radius);
+        Log.d("MARK2","marker.radius == "+marker.radius);
 
-        return distance < marker.radius;
-
+        //idk why?
+        return distance <= 1;//marker.radius;
     }
 }
