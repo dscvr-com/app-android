6549fd895499718c04f7a5d985ee63747224f79d
marckuz <maelbambo@gmail.com>
2016-10-12 13:36:38 +0800

    -removed unnecessary committed files

--- ndkmodule/src/main/obj/local/armeabi-v7a/objs-debug/ndkmodule/online-stitcher/src/common/image.o.d
+++
@@ -1,2 +0,0 @@
-/Users/mcandres/Documents/sandbox/optonaut/android2/app-android/ndkmodule/src/main/obj/local/armeabi-v7a/objs-debug/ndkmodule/online-stitcher/src/common/image.o: \
- /Users/mcandres/Documents/sandbox/optonaut/android2/app-android/ndkmodule/src/main/jni/online-stitcher/src/common/image.cpp
